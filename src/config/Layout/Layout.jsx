217b5a19f884dce163627f5b97c1e898f20c3b91
Ali Jendoubi <alijandhd@gmail.com>
2022-11-01 14:53:40 +0100

    Setup Themes

--- src/config/Layout/Layout.jsx
+++ src/config/Layout/Layout.jsx
@@ -3,11 +3,11 @@ import BottomNav from './components/BottomNav';
 
 function Layout({ children }) {
   return (
-    <div className="main antialiased font-display flex justify-center align-center items-center h-screen">
+    <div data-theme="wireframe" className="main antialiased font-display flex justify-center align-center items-center h-screen">
       <div className="mockup-phone">
         <div className="camera" />
         <div className="display overflow-hidden">
-          <div className="artboard p-5 relative overflow-hidden bg-white phone-3">
+          <div className="artboard p-5 relative  bg-base-100 overflow-hidden phone-3">
             {children}
             <BottomNav />
           </div>
