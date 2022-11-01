a44f30fe790a8030e44b3022b13e8d281f2a981e
Ali Jendoubi <alijandhd@gmail.com>
2022-11-01 17:01:19 +0100

    Install framer motion & add apps

--- src/config/Layout/Layout.jsx
+++ src/config/Layout/Layout.jsx
@@ -6,8 +6,13 @@ function Layout({ children }) {
     <div data-theme="wireframe" className="main antialiased font-display flex justify-center align-center items-center h-screen">
       <div className="mockup-phone">
         <div className="camera" />
-        <div className="display overflow-hidden">
-          <div className="artboard p-5 relative  bg-base-100 overflow-hidden phone-3">
+        <div className="display  overflow-hidden">
+          <div
+            // style={{
+            //   backgroundImage: 'url(https://preview.redd.it/gdsbdf97ihq31.png?width=640&crop=smart&auto=webp&s=fcb5c1ed83c93580a4ddf97de06b7c606dd7d359)', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover',
+            // }}
+            className="artboard p-9 relative  bg-base-100 overflow-hidden phone-3"
+          >
             {children}
             <BottomNav />
           </div>
