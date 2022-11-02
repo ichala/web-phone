5ae886fe45708898051d91e73d12c0ba64f143d4
Ali Jendoubi <alijandhd@gmail.com>
2022-11-02 10:28:39 +0100

    Add A 2 sec Loader

--- src/screens/Pages/Calculator/Calculator.jsx
+++ src/screens/Pages/Calculator/Calculator.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Calculators from 'awesome-react-calculator';
 
 function Calculator() {
-//   function handleInput(input) {
-//     console.log(`${input.expression} is shown in the calculator, User clicked the ${input.key}`);
-//   }
+  const [Loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
-  //   function onResultChange(newResult) {
-  //     console.log(newResult);
-  //     console.log(`${newResult.expression} is validated as ${newResult.result} `);
-  //   }
   return (
     <motion.div
       className="absolute top-0 left-0 bg-gradient-to-t from-gray-200 to-blue-700 h-full w-full"
@@ -22,18 +21,22 @@ function Calculator() {
         ease: [0, 0.71, 0.2, 1.01],
       }}
     >
-      {/* <div className="flex justify-center items-center h-full w-full">
-        <div className="animate-pulse text-4xl text-white">
-          <div className="avatar">
-            <div className="w-16 mask mask-squircle">
-              <img src="/assets/images/calculator.png" alt="weather-app" />
+      {Loading ? (
+        <div className="flex justify-center items-center h-full w-full">
+          <div className="animate-pulse text-4xl text-white">
+            <div className="avatar">
+              <div className="w-16 mask mask-squircle">
+                <img src="/assets/images/calculator.png" alt="weather-app" />
+              </div>
             </div>
           </div>
         </div>
-      </div> */}
-      <div className="calculator-demo h-[685px] overflow-hidden w-full">
-        <Calculators />
-      </div>
+      )
+        : (
+          <div className="calculator-demo h-[685px] overflow-hidden w-full">
+            <Calculators />
+          </div>
+        ) }
     </motion.div>
   );
 }
