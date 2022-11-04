b3dcdb24b6e2b07b756ac17991f3b5c41155a4b9
Ali Jendoubi <alijandhd@gmail.com>
2022-11-04 14:32:53 +0100

    remove navigate

--- src/screens/Settings/Settings.jsx
+++ src/screens/Settings/Settings.jsx
@@ -2,13 +2,11 @@ import React, { useContext } from 'react';
 import { CgDarkMode } from 'react-icons/cg';
 import { BiLogOutCircle } from 'react-icons/bi';
 import { signOut } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
 import { UserDataContext } from '../../config/UserData/storage';
 import { auth } from '../../config/firebase';
 
 function Settings() {
   const { UserData, setUserData } = useContext(UserDataContext);
-  const navigate = useNavigate();
 
   return (
     <div className="absolute left-0  h-full w-full mb-10">
@@ -38,7 +36,6 @@ function Settings() {
             className="flex justify-center items-center gap-1 bg-transparent hover:bg-error hover:text-error-content"
             onClick={() => {
               signOut(auth);
-              navigate('/login');
             }}
           >
             <BiLogOutCircle size={20} />
