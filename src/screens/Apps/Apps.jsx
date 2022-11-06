9ad6e8d58518907e402108820b43e01edaa82d45
Ali Jendoubi <alijandhd@gmail.com>
2022-11-06 12:14:42 +0100

    Add CryptoApp apps button

--- src/screens/Apps/Apps.jsx
+++ src/screens/Apps/Apps.jsx
@@ -39,6 +39,13 @@ function Apps() {
           </div>
         </div>
       </Link>
+      <Link to="/cryptoapp">
+        <div className="avatar">
+          <div className="w-16 mask mask-squircle">
+            <img src="/assets/images/cryptoapp.png" alt="Jokes App" />
+          </div>
+        </div>
+      </Link>
 
       {/* <div className="avatar">
         <div className="w-16 mask mask-squircle">
