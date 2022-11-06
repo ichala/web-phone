7f267ef8c7949814c28c97a2cc137c8f9f4a6446
Ali Jendoubi <alijandhd@gmail.com>
2022-11-06 12:27:19 +0100

    Add coins cards animation

--- src/screens/Pages/CryptoApp/Components/CoinMin.jsx
+++ src/screens/Pages/CryptoApp/Components/CoinMin.jsx
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+import { motion } from 'framer-motion';
+
 import {
   BsArrowDownShort, BsArrowUpRightSquare, BsArrowUpShort, BsStar, BsStarFill,
 } from 'react-icons/bs';
@@ -22,30 +24,41 @@ function CoinMin({ data }) {
     }
   };
   return (
-
-    <div className={`w-1/3 cursor-pointer text-white bg-gradient-to-t hover:bg-gradient-to-t ${data.change >= 0 ? ' from-slate-900 to-green-900  hover:from-slate-900 hover:to-green-700' : 'bg-gradient-to-t from-slate-900 to-red-900 hover:from-slate-900 hover:to-red-700'}  h-[160px] glass`}>
-      <div className="flex justify-between p-1">
-        <button type="button" onClick={() => handlefavorite(data.uuid)}>{UserData.CryptoApp.fav.includes(data.uuid) ? <BsStarFill className="text-yellow-200" /> : <BsStar />}</button>
-        <BsArrowUpRightSquare />
-      </div>
-      <div className="flex justify-center mt-2">
-        <div className="avatar">
-          <div className="w-20 mask mask-squircle">
-            <img src={data.iconUrl} className="avatar" alt="" />
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ once: true }}
+      className={`w-1/3 cursor-pointer text-white bg-gradient-to-t hover:bg-gradient-to-t ${data.change >= 0 ? ' from-slate-900 to-green-900  hover:from-slate-900 hover:to-green-700' : 'bg-gradient-to-t from-slate-900 to-red-900 hover:from-slate-900 hover:to-red-700'}  h-[160px] glass`}
+      transition={{
+        duration: 0.2,
+        delay: 0.1,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    >
+      <div>
+        <div className="flex justify-between p-1">
+          <button type="button" onClick={() => handlefavorite(data.uuid)}>{UserData.CryptoApp.fav.includes(data.uuid) ? <BsStarFill className="text-yellow-200" /> : <BsStar />}</button>
+          <BsArrowUpRightSquare />
+        </div>
+        <div className="flex justify-center mt-2">
+          <div className="avatar">
+            <div className="w-20 mask mask-squircle">
+              <img src={data.iconUrl} className="avatar" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <div className="flex flex-col items-end mr-3">
+            <p className="text-md font-bold">{data.symbol}</p>
+            <p className="text-xs flex ">
+              {data.change}
+              %
+              {data.change >= 0 ? <BsArrowUpShort size={13} /> : <BsArrowDownShort size={13} />}
+            </p>
           </div>
         </div>
       </div>
-      <div className="flex justify-end">
-        <div className="flex flex-col items-end mr-3">
-          <p className="text-md font-bold">{data.symbol}</p>
-          <p className="text-xs flex ">
-            {data.change}
-            %
-            {data.change >= 0 ? <BsArrowUpShort size={13} /> : <BsArrowDownShort size={13} />}
-          </p>
-        </div>
-      </div>
-    </div>
+    </motion.div>
   );
 }
 
