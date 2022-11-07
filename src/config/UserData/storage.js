072dba0d0f19d55db40db8e6cd9380d16c394ff9
Ali Jendoubi <alijandhd@gmail.com>
2022-11-07 13:44:10 +0100

    Add storage wallpaper

--- src/config/UserData/storage.js
+++ src/config/UserData/storage.js
@@ -3,7 +3,7 @@ import { useState, createContext } from 'react';
 export const UserDataContext = createContext();
 
 export const UserDataProvider = ({ children }) => {
-  const [UserData, setUserData] = useState(JSON.parse(localStorage.getItem('UserData')) || { darkmode: false });
+  const [UserData, setUserData] = useState(JSON.parse(localStorage.getItem('UserData')) || { darkmode: false, settings: { wallpaper: { link: 'default', id: 0 } } });
   localStorage.setItem('UserData', JSON.stringify(UserData));
   return (
     <UserDataContext.Provider value={{ UserData, setUserData }}>
