1762e0fd804d25c1e8bb4bf23992eea4345c5d53
Ali Jendoubi <alijandhd@gmail.com>
2022-11-08 10:24:51 +0100

    Add Home page layout & Design

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,15 +1,32 @@
 import moment from 'moment';
-import React from 'react';
+import React, { useContext } from 'react';
+import { BsGithub } from 'react-icons/bs';
+
+import { AuthContext } from '../../config/Auth/context';
 
 function Home() {
   const time = moment().format('HH:MM');
   const date = moment().format('MMM Do YY');
+  const user = useContext(AuthContext).currentUser;
+  console.log(user);
   return (
 
-    <div className="absolute top-0 left-0 glass p-5 h-full w-full">
-      <h1 className=" text-7xl font-bold text-white">Hello There !</h1>
+    <div className="absolute top-0 left-0 z-0 p-5 h-full w-full text-white">
+      <h1 className=" text-7xl font-bold text-white mt-4">Hello There !</h1>
       <h2 className=" text-5xl mt-10 text-center text-white">{time}</h2>
       <h3 className=" text-2xl text-center text-white">{date}</h3>
+      <div className=" flex flex-col items-center justify-center mt-4">
+        <div className="avatar ">
+          <div className="w-24 mask mask-hexagon">
+            <img alt={user.name || ''} src={user.image} referrerPolicy="no-referrer" />
+          </div>
+        </div>
+        <p className="text-2xl">{user.name || user.email }</p>
+        <a href="https://github.com/ichala/web-phone" target="_blank" className="btn gap-2 mt-10 rounded uppercase" rel="noreferrer">
+          <BsGithub size={20} />
+          Source Code
+        </a>
+      </div>
     </div>
   );
 }
