8633f3b63b90bd635a2c7c2dd69da3e9b857e20c
Ali Jendoubi <alijandhd@gmail.com>
2022-11-13 12:46:24 +0100

    Add contacts design

--- src/screens/Pages/Contacts/Components/AllContacts.jsx
+++ src/screens/Pages/Contacts/Components/AllContacts.jsx
@@ -7,12 +7,27 @@ import { UserDataContext } from '../../../../config/UserData/storage';
 
 function AllContacts() {
   const { UserData } = useContext(UserDataContext);
-  const ContactList = UserData.contacts;
+  const [FiltredContacts, setFiltredContacts] = useState(UserData.contacts);
   const [Filter, setFilter] = useState('');
+  function filterContacts(filter) {
+    setFilter(filter);
+    if (filter !== '') {
+      setFiltredContacts(FiltredContacts.filter(
+        (contact) => contact.fullname.toLowerCase().includes(filter.toLowerCase())
+         || contact.email.toLowerCase().includes(filter.toLowerCase())
+          || contact.mobile.toLowerCase().includes(filter.toLowerCase()),
+      ));
+    } else {
+      setFiltredContacts(UserData.contacts);
+    }
+  }
   return (
     <div>
       <div className="flex justify-between mt-7 p-3 text-gray-400 items-center">
-        <AiOutlineLeft size={30} />
+        <Link to="/apps">
+          <AiOutlineLeft size={30} />
+        </Link>
+
         <div className="text-2xl text-center italic  subpixel-antialiased  text-black font-light">My Contacts</div>
         <Link to="/contacts/new">
           <AiOutlinePlusCircle size={30} />
@@ -22,13 +37,13 @@ function AllContacts() {
       <div className="flex justify-center ">
         <div className="flex justify-center w-[340px] bg-white bg-white text-gray-400 shadow-lg rounded-full p-2">
           <AiOutlineSearch size={30} />
-          <input type="text" name="search-contact" id="search-contact" value={Filter} className="input w-full input-sm focus:outline-0 bg-transparent border-0" onChange={(e) => setFilter(e.target.value)} />
-          {Filter && <AiOutlineCloseCircle className="cursor-pointer" onClick={() => setFilter('')} size={30} /> }
+          <input type="text" name="search-contact" id="search-contact" value={Filter} className="input w-full input-sm focus:outline-0 bg-transparent border-0" onChange={(e) => filterContacts(e.target.value)} />
+          {Filter && <AiOutlineCloseCircle className="cursor-pointer" onClick={() => { setFilter(''); setFiltredContacts(UserData.contacts); }} size={30} /> }
         </div>
       </div>
-      <div className="h-[475px]  overflow-y-scroll no-scrollbar bg-white shadow-2xl rounded-lg mt-5 m-2">
+      <div className="h-[475px]  overflow-y-scroll no-scrollbar bg-white shadow-md rounded-lg mt-5 m-2">
         <ul className="menu font w-full  text-black divide-y">
-          {ContactList.map((contact) => (
+          {FiltredContacts.map((contact) => (
             <li key={contact.id} className="hover-bordered font-normal italic subpixel-antialiased">
               <Link to={`/contacts/${contact.id}`}>
                 <div className="avatar ">
